fc7e83614eefdb048057590aacf428ca48a2e795
Gareth M. <gkm25@protonmail.com>
2025-11-19 15:42:31 -0600

    demo dashboard

--- cesium/src/websocket.js
+++ cesium/src/websocket.js
@@ -12,7 +12,14 @@ export function connectWebSocket() {
     console.log("WebSocket Connected:", frame);
 
     stompClient.subscribe("/topic/scenarios", (message) => {
-      const body = JSON.parse(message.body);
+      const body = JSON.parse(JSON.parse(message.body));
+      const newRow = "<tr>\n<td>"+body.type+"</td>\n<td>"+body.polygonCoords+"</td>\n</tr>";
+     // var p = document.createElement("p")
+      var r = document.createTextNode("event "+body.type+" with "+body.polygonCoords)
+      //p.appendChild(r)
+      const scenarios = document.getElementById("scenario-status")
+      scenarios.appendChild(r);
+      
       console.log(body);
     });
   };
